df0d9a6ea73b8992518d33cf0c247ba9fb25d3d7
kanmii <kanmii@kanmii1.fritz.box>
2018-08-05 11:52:31 +0200

    More styles for shift list in view.

--- lib/burda_web/views/index_view.ex
+++ lib/burda_web/views/index_view.ex
@@ -1,7 +1,10 @@
 defmodule BurdaWeb.IndexView do
   use BurdaWeb, :view
 
+  alias Burda.Shift
+
   @iso_time "{ISOtime}"
+  @new_decimal Decimal.new("0.00")
 
   @spec format_time_iso(Time.t()) :: binary()
   def format_time_iso(%Time{} = time),
@@ -9,4 +12,8 @@ defmodule BurdaWeb.IndexView do
       time
       |> Timex.format!(@iso_time)
       |> String.slice(0, 8)
+
+  @spec total_earnings(nonempty_list(%Shift{})) :: Decimal.t()
+  def total_earnings(shifts) when is_list(shifts),
+    do: Enum.reduce(shifts, @new_decimal, &Decimal.add(&1.total_pay, &2))
 end
