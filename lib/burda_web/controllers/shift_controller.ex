23146cceb6bbc707af65e8f026e87affc4517298
maneptha <maneptha@gmail.com>
2018-08-09 10:21:41 +0200

    Render meta list to new shift form.

--- lib/burda_web/controllers/shift_controller.ex
+++ lib/burda_web/controllers/shift_controller.ex
@@ -1,15 +1,20 @@
 defmodule BurdaWeb.ShiftController do
   use Phoenix.Controller
 
+  alias Burda.Meta.Api, as: MetaApi
+
   @page_css "routes/shift.css"
   @page_js "routes/shift.js"
-  @new_meta_form_css "components/new-meta-form.css"
 
   plug(:assign_defaults)
 
   @spec new(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def new(conn, _params) do
-    render(conn, "new-shift.html", new_meta_form_css: @new_meta_form_css)
+    render(
+      conn,
+      "new-shift.html",
+      metas: MetaApi.list()
+    )
   end
 
   @spec assign_defaults(Plug.Conn.t(), any()) :: Plug.Conn.t()
