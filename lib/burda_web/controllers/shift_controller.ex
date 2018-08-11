3ab548919279db4e7d741403d5e9a64f1e54a0a4
maneptha <maneptha@gmail.com>
2018-08-11 14:41:55 +0200

    Building new shift form - wip.

--- lib/burda_web/controllers/shift_controller.ex
+++ lib/burda_web/controllers/shift_controller.ex
@@ -6,14 +6,76 @@ defmodule BurdaWeb.ShiftController do
   @page_css "routes/shift.css"
   @page_js "routes/shift.js"
 
+  @months_of_year [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec"
+                  ]
+                  |> Enum.with_index(1)
+                  |> Enum.map(fn {m, index} ->
+                    {
+                      index,
+                      %{
+                        display: m,
+                        selected: ""
+                      }
+                    }
+                  end)
+                  |> Enum.into(%{})
+
+  @days_of_month 1..31
+                 |> Enum.map(
+                   &{&1,
+                    %{
+                      display: String.pad_leading("#{&1}", 2, "0"),
+                      selected: ""
+                    }}
+                 )
+                 |> Enum.into(%{})
+
   plug(:assign_defaults)
 
   @spec new(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def new(conn, _params) do
+    today = Date.utc_today()
+
+    months_of_year =
+      Map.update!(
+        @months_of_year,
+        today.month,
+        &Map.put(&1, :selected, "selected")
+      )
+
+    days_of_month =
+      Map.update!(
+        @days_of_month,
+        today.day,
+        &Map.put(&1, :selected, "selected")
+      )
+
+    year = today.year
+
+    years =
+      -4..-1
+      |> Enum.map(&{&1 + year, ""})
+      |> Enum.concat([{year, "selected"}])
+
     render(
       conn,
       "new-shift.html",
-      metas: MetaApi.list()
+      metas: MetaApi.list(),
+      months_of_year: months_of_year,
+      days_of_month: days_of_month,
+      years: years
     )
   end
 
