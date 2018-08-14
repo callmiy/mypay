6d956683ec5e3f0f70c5a56b1c9b6f877b8c2dc3
maneptha <maneptha@gmail.com>
2018-08-14 19:24:07 +0200

    Removed event listener 'touchstart', 'click' is enough.

--- front-end/src/app.ts
+++ front-end/src/app.ts
@@ -75,35 +75,18 @@ const processSidebar = () => {
     const sidebarTriggerEl = document.getElementById("sidebar-trigger");
 
     if (containerEl && sidebarTriggerEl) {
-      containerEl.addEventListener(
-        "click",
-        evt => {
-          evt.stopPropagation();
-          sidebar.classList.remove("visible");
-        },
-        false
-      );
+      containerEl.addEventListener("click", evt => {
+        evt.stopPropagation();
+        sidebar.classList.remove("visible");
+      });
 
-      containerEl.addEventListener(
-        "touchstart",
-        evt => {
-          evt.stopPropagation();
-          sidebar.classList.remove("visible");
-        },
-        false
-      );
-
-      sidebarTriggerEl.addEventListener(
-        "click",
-        evt => {
-          evt.preventDefault();
-          evt.stopPropagation();
-          sidebar.classList[
-            sidebar.classList.contains("visible") ? "remove" : "add"
-          ]("visible");
-        },
-        false
-      );
+      sidebarTriggerEl.addEventListener("click", evt => {
+        evt.preventDefault();
+        evt.stopPropagation();
+        sidebar.classList[
+          sidebar.classList.contains("visible") ? "remove" : "add"
+        ]("visible");
+      });
     }
   };
 
