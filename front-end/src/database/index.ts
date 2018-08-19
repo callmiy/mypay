b5beb57c5c354ca174d2aa3ea20ed0c60018d122
maneptha <maneptha@gmail.com>
2018-08-19 19:14:22 +0200

    Added pouchdb debug. Can destroy old pouchdb database.
    By bumping the pouch db version number, we can destroy a pouchdb
    database and create a new one.

--- front-end/src/database/index.ts
+++ front-end/src/database/index.ts
@@ -1,18 +1,50 @@
 import PouchDB from "pouchdb-browser";
 import PouchDBFind from "pouchdb-find";
+import PouchdbDebug from "pouchdb-debug";
 
-PouchDB.plugin(PouchDBFind);
+const POUCH_DB_OLD_VERSION = 0;
+const POUCH_DB_CURRENT_VERSION = 1;
+const getDbName = (version: number) => "WORKER-WAGES-maneptha-" + version;
 
 class Database {
-  db = new PouchDB("WORKER-WAGES-maneptha");
+  db: PouchDB.Database;
+  private name: string;
 
   constructor() {
+    this.destroyOldDb();
+    this.name = getDbName(POUCH_DB_CURRENT_VERSION);
+    this.db = new PouchDB(this.name);
+    PouchDB.plugin(PouchDBFind);
+    PouchDB.plugin(PouchdbDebug);
+
     this.db.createIndex({
       index: { fields: ["schemaType"] }
     });
+
+    this.onStart();
   }
 
   genId = () => new Date().toJSON().slice(0, -1) + "000Z";
+
+  destroyOldDb = async () => {
+    const dbName = getDbName(POUCH_DB_OLD_VERSION);
+
+    try {
+      await new PouchDB(dbName).destroy();
+    } catch (error) {
+      // tslint:disable-next-line:no-console
+      console.log("Error destroying PouchDb instance: ", dbName);
+    }
+  };
+
+  async onStart() {
+    const info = await this.db.info();
+    // tslint:disable-next-line:no-console
+    console.log(
+      `Pouchdb info for "${this.name}":\n`,
+      JSON.stringify(info, null, 2)
+    );
+  }
 }
 
 const db = new Database();
--- front-end/src/templates.d.ts
+++ front-end/src/templates.d.ts
@@ -1,2 +1,3 @@
 declare module "*.hbs";
 declare module "*.handlebars";
+declare module 'pouchdb-debug'
