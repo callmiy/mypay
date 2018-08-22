778d1f52459392eb3f7ddf8d73add97b16556a1e
maneptha <maneptha@gmail.com>
2018-08-22 22:11:57 +0200

    formatTimeAmPm handlebars helper - changed time format.

--- front-end/src/templates/helpers/formatTimeAmPm.ts
+++ front-end/src/templates/helpers/formatTimeAmPm.ts
@@ -5,5 +5,5 @@ import * as moment from "moment";
  */
 export default (time: string) => {
   const parsedTime = moment(time, moment.HTML5_FMT.TIME_SECONDS);
-  return parsedTime.format("HH:mm A");
+  return parsedTime.format("hh:mm A");
 };
