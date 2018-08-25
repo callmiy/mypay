d6201beacf5555f7d6627110385c269bf823de3e
maneptha <maneptha@gmail.com>
2018-08-25 10:37:54 +0200

    Introduced Jest testing - but test too slow.

---
+++ front-end/src/utils/utils.test.ts
@@ -0,0 +1,5 @@
+import { sortByDateTime } from "./utils";
+
+it("sorts by date time", () => {
+  expect(sortByDateTime()).toBe(12);
+});
--- front-end/src/utils/utils.ts
+++ front-end/src/utils/utils.ts
@@ -1,3 +1,5 @@
+import * as moment from "moment";
+
 export const capitalize = (val: string) => {
   return val.charAt(0).toUpperCase() + val.slice(1);
 };
@@ -7,3 +9,7 @@ export const docReady = (fn: () => void) =>
 
 export const isServerRendered = () =>
   !!document.querySelector('[name="3snsaaPmwVPzy6mFtib"]');
+
+export const sortByDateTime = () => {
+  return moment("1921-12-18T04:31:15.600000");
+};
